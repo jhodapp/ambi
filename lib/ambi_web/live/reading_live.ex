048193609fd66516d6f710d95839163009dbaa33
Jim Hodapp <james.hodapp@gmail.com>
2021-02-13 17:02:13 -0600

    Added a basic tile-based Bulma CSS based frontend.

--- lib/ambi_web/live/reading_live.ex
+++ lib/ambi_web/live/reading_live.ex
@@ -7,44 +7,17 @@ defmodule AmbiWeb.ReadingLive do
     Logger.debug "ReadingLive.mount() called, subscribing PubSub topic"
     Ambi.subscribe()
 
-    {:ok, assign(socket, %{reading: Ambi.get_reading()})}
+    socket = assign(socket, %{reading: Ambi.get_reading()})
+    {:ok, socket}
   end
 
   def handle_info(:added, socket) do
     Logger.debug "Received :added event message"
-    #{:noreply, assign(socket, %{reading: Ambi.get_reading()})}
-    {:noreply, assign(socket, %{})}
+    {:noreply, assign(socket, %{reading: Ambi.get_reading()})}
   end
 
   def handle_info(:reading_refresh, socket) do
     Logger.debug "Received :refresh_reading event message"
     {:noreply, assign(socket, %{reading: Ambi.get_reading()})}
-  end
-
-  def render(assigns) do
-    ~L"""
-    <section class="phx-hero">
-      <h1>Live Readings</h1>
-      <p>
-        Current temp: <%= @reading.temperature |> Decimal.from_float() |> Decimal.round(1) %> C <br/>
-        Current humidity: <%= @reading.humidity  |> Decimal.from_float() |> Decimal.round(1) %>% <br/>
-        Current pressure: <%= @reading.pressure %> mb <br/>
-        Current dust concentration: <%= @reading.dust_concentration |> Decimal.from_float() |> Decimal.round(0) %> pieces/liter <br/>
-        Current air purity: <%= @reading.air_purity %> <br/>
-        <hr class="dotted"/>
-        Average temp: <%= @reading.average_temp |> Decimal.from_float() |> Decimal.round(1) %> C <br/>
-        Max temp: <%= @reading.max_temp |> Decimal.from_float() |> Decimal.round(1) %> C <br/>
-        Min temp: <%= @reading.min_temp |> Decimal.from_float() |> Decimal.round(1) %> C <br/>
-        <hr class="dotted"/>
-        Average humidity: <%= @reading.average_humidity |> Decimal.from_float() |> Decimal.round(1) %>%  <br/>
-        Max humidity: <%= @reading.max_humidity |> Decimal.from_float() |> Decimal.round(1) %>% <br/>
-        Min humidity: <%= @reading.min_humidity |> Decimal.from_float() |> Decimal.round(1) %>% <br/>
-        <hr class="dotted"/>
-        <%= @reading.readings_count %> readings <br/>
-        First: <%= @reading.first_inserted_at %> <br/>
-        Last: <%= @reading.last_inserted_at %>
-      </p>
-    </section
-    """
   end
 end
